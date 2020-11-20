7b04baa2b2ce080b7450795a1785ff862c3ebd07
Paweł Przytuła <pprzytula@outlook.com>
2020-11-20 22:59:51 +0100

    Added ApplicationDbContext

---
+++ BlazorCRUD/BlazorCRUD/Data/EmployeeInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public class EmployeeInfo
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Department { get; set; }
+
+    }
+}
